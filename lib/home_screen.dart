e931abb4de66d41fbcf9870c92034e34ae85d8ff
Lawitadelimon <lau72abrvjua@gmail.com>
2024-02-16 12:07:16 -0600

    Adding Theme

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,32 +10,30 @@ class HomeScreen extends StatelessWidget {
           title: const Text('Componentes de Flutter'),
         ),
         body: ListView(
-          children: const [
+          children:  [
               ListTile(
-              leading: Icon(Icons.wordpress_outlined),
+              leading: const Icon(Icons.wordpress_outlined),
               title: Text('GSW campeones NBA',
-              style: TextStyle(color: Color.fromARGB(255, 7, 57, 255), fontWeight: FontWeight.bold),
+              style: Theme.of(context).textTheme.headlineLarge,
               ),
-              subtitle: Text('2020-07-21 - Chase Center'),
-              trailing: Icon(Icons.add_location_alt_outlined),
+              subtitle: const Text('2020-07-21 - Chase Center'),
+              trailing: const Icon(Icons.add_home_work_outlined),
             ),
-            Divider(),
-              ListTile(
-              leading: Icon(Icons.account_balance_sharp),
+            const Divider(),
+              const ListTile(
+              leading: Icon(Icons.accessibility_outlined),
               title: Text('Curry el mejor jugador',
-              style: TextStyle(color: Color.fromARGB(255, 7, 57, 255), fontWeight: FontWeight.bold),
               ),
               subtitle: Text('Campeon MVP'),
-              trailing: Icon(Icons.add_location_alt_outlined),
+              trailing: Icon(Icons.assistant_outlined),
             ),
-            Divider(),
-              ListTile(
-              leading: Icon(Icons.wordpress_outlined),
+            const Divider(),
+              const ListTile(
+              leading: Icon(Icons.waving_hand),
               title: Text('Splash Brothers',
-              style: TextStyle(color: Color.fromARGB(255, 7, 57, 255), fontWeight: FontWeight.bold),
               ),
               subtitle: Text('Klay & Curry'),
-              trailing: Icon(Icons.add_location_alt_outlined),
+              trailing: Icon(Icons.wb_incandescent),
             ),
           ]
         ),
--- lib/main_app.dart
+++ lib/main_app.dart
@@ -8,9 +8,21 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: const HomeScreen(),
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 156, 228, 240),
+        appBarTheme: const AppBarTheme(color: Color.fromARGB(235, 27, 126, 154),),
+        textTheme: const TextTheme(
+          headlineLarge: TextStyle(
+            color: Colors.black,
+            fontSize: 25.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'sans-serif',
+          )
+        ),
+      ),
     );
   }
 }
